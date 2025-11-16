05a42b2eead58ae0f496324c02ed48511aefd4b7
Josef Gisis <josefgisis@gmail.com>
2025-11-16 11:10:35 -0500

    improve end of argument logging

--- src/main.c
+++ src/main.c
@@ -5,18 +5,22 @@
 #define FALSE 0
 
 int main(const int argc, char **argv) {
-    char *key = malloc(1);
-    char *value = malloc(1);
+    char *key = malloc(100);
+    char *value = malloc(100);
 
     for (int i=1; i < argc; i++) {
         const int result = get_opt_args(argc, argv, key, value);
 
-        if (result == 1) {
+        if (result == 0) {
+            printf("Reached end of optional arguments\n");
+        } else if (result == 1) {
             printf("key=%s value=%s\n", key, value);
-        } else if (result == 0) {
-            printf("Reached end of arguments\n");
         } else if (result == -1) {
             printf("Failed to parse output\n");
+        }
+
+        if (i == argc -1 && result != 0) {
+            printf("Reached end of arguments\n");
         }
     }
 
@@ -24,4 +28,4 @@ int main(const int argc, char **argv) {
     free(value);
 
     return 0;
-}
+};
