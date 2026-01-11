8fc2bde0fa0c6195edd4d71d22bb258aa724e1f5
Josef Gisis <josefgisis@gmail.com>
2026-01-10 23:37:16 -0500

    improve end of argument logging

--- src/main.c
+++ src/main.c
@@ -3,26 +3,41 @@
 #include "parse-args.h"
 #define TRUE 1
 #define FALSE 0
+#define MAX_ARGS_LENGTH 100
 
 int main(const int argc, char **argv) {
-    char *key = malloc(100);
-    char *value = malloc(100);
+    char *key = malloc(1);
+    char *value = malloc(1);
 
-    for (int i=1; i < argc; i++) {
+    for (int i = 1, end_of_args = FALSE; i <= MAX_ARGS_LENGTH && end_of_args == FALSE; i++) {
         const int result = get_opt_args(argc, argv, key, value);
 
         if (result == 0) {
+            end_of_args = TRUE;
             printf("Reached end of optional arguments\n");
         } else if (result == 1) {
             printf("key=%s value=%s\n", key, value);
         } else if (result == -1) {
             printf("Failed to parse output\n");
         }
-
-        if (i == argc -1 && result != 0) {
-            printf("Reached end of arguments\n");
-        }
     }
+
+    //
+    // for (int i=TRUE; i == TRUE ; i++) {
+    //     const int result = get_opt_args(argc, argv, key, value);
+    //
+    //     if (result == 0) {
+    //         printf("Reached end of optional arguments\n");
+    //     } else if (result == 1) {
+    //         printf("key=%s value=%s\n", key, value);
+    //     } else if (result == -1) {
+    //         printf("Failed to parse output\n");
+    //     }
+    //
+    //     if (i == argc -1 && result != 0) {
+    //         printf("Reached end of arguments\n");
+    //     }
+    // }
 
     free(key);
     free(value);
--- src/parse-args.c
+++ src/parse-args.c
@@ -1,48 +1,75 @@
 #include "parse-args.h"
 #include <ctype.h>
+#include <stdlib.h>
 #include <_string.h>
+#include <stdio.h>
 #define EOA 0
 #define ERROR (-1)
 #define SUCCESS 1
 #define MAX_VALUE_LENGTH 1000
 
 int get_opt_arg_index = 1;
+int get_opt_arg_bookmark = 0;
 
-int get_opt_args(const int argc, char * argv[], char * key, char * value) {
+int get_opt_args(const int argc, char *argv[], char *key, char *value) {
+    if (get_opt_arg_bookmark > 0) {
+        const char current_char = argv[get_opt_arg_index][get_opt_arg_bookmark++];
+        if (current_char == '\0' || current_char == '\n' || current_char == '=') {
+            get_opt_arg_bookmark = 0;
+            get_opt_arg_index++;
+        } else {
+            *key = current_char;
+            value[0] = '\0';
+            return SUCCESS;
+        }
+    }
+
     if (argc == 1 || get_opt_arg_index >= argc) {
         return EOA;
     }
 
     // Skip all non-optional arguments up until the end of argv
-    for (; get_opt_arg_index < argc && argv[get_opt_arg_index][0] != '-'; get_opt_arg_index++)
-        ;
+    for (; get_opt_arg_index < argc && argv[get_opt_arg_index][0] != '-'; get_opt_arg_index++);
 
     // If we have traversed the whole list.
     if (get_opt_arg_index >= argc) return EOA;
 
     // Move through the arg until the last -
-    int arg_pos = 0;
-    for (; argv[get_opt_arg_index][arg_pos] == '-'; arg_pos++)
-        ;
 
-    // The user could have passed just ---
-    if (argv[get_opt_arg_index][arg_pos] == '\0') {
-        return ERROR;  // Invalid option format
+    for (; argv[get_opt_arg_index][get_opt_arg_bookmark] == '-'; get_opt_arg_bookmark++) {
+        if (get_opt_arg_bookmark > 2) return ERROR;
     }
 
-    *key = argv[get_opt_arg_index][arg_pos];
+    if (argv[get_opt_arg_index][get_opt_arg_bookmark] == '\0') {
+        return ERROR;
+    }
 
-    const char * value_segment = strchr(argv[get_opt_arg_index], '=');
+    if (get_opt_arg_bookmark == 1) {
+        value[0] = '\0';
+        const char current_char = argv[get_opt_arg_index][get_opt_arg_bookmark++];
+
+        if (current_char == '\0' || current_char == '\n' || current_char == '=') {
+            get_opt_arg_bookmark = 0;
+            return ERROR;
+        }
+
+        *key = current_char;
+        return SUCCESS;
+    }
+
+    *key = argv[get_opt_arg_index][get_opt_arg_bookmark++];
+    const char *value_segment = strchr(argv[get_opt_arg_index], '=');
     if (value_segment == NULL) {
         value[0] = '\0';
     } else {
         strncpy(value, value_segment + 1, MAX_VALUE_LENGTH);
     }
-
     get_opt_arg_index++;
+    get_opt_arg_bookmark = 0;
+
     return SUCCESS;
 }
 
-int get_pos_args(int argc, char *argv[], char * pattern, char ** results) {
+int get_pos_args(int argc, char *argv[], char *pattern, char **results) {
     return SUCCESS;
 }
